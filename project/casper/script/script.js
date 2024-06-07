ae1873cf7919e57df2ac43046b0f4659a6b87d94
yeonwoo <165612452+ch0120yy@users.noreply.github.com>
2024-06-07 18:04:58 +0900

    수업 casper 0607

--- project/casper/script/script.js
+++ project/casper/script/script.js
@@ -38,3 +38,40 @@ nav.addEventListener('mouseout',()=>{
         // i.style.overflow = 'hidden'
     }
 })
+
+// ========================== 모바일 nav js
+// m_nav 상단 변수 존재함
+// 1. 모바일 전체메뉴 아이콘(m_nav > all_nav) 클릭 시
+// 1-1. 모바일 nav가(m_nav > nav) 보이는 상태라면 -> 숨기기
+// 1-2. 모바일 nav가 숨겨진 상태라면 -> 보이기
+const all_nav_m = m_nav.querySelector('.all_nav')
+const nav_m = m_nav.querySelector('nav')
+let boolean = false; // 안 보이는 상태(기본)
+// nav_m.classList.add('m_nav_hide') // 숨김값 클래스 적용
+console.log(all_nav_m, nav_m) // ok
+all_nav_m.addEventListener('click',(e)=>{
+    e.preventDefault(); //a herf 막기 확인 시 스크롤 내리고 클릭하면 스크롤이 안 올라갔을 때 제대로 적용된 것
+    // ==================== 클래스(classList.toggle)
+    // nav_m.classList.toggle('m_nav_hide')
+    // ==================== 조건문(if)
+    // boolean = !boolean
+    /* if(boolean==true){nav_m.style.display = 'block'}else{nav_m.style.display = 'none'} */
+    // 삼항조건연산자 조건? 참 : 거짓결과; (단순할 때 사용)
+    // nav_m.style.display = boolean ? 'block' : 'none'
+})
+// 2. 모바일 서브메뉴 모두 숨기기(기본)
+const sub_m = nav_m.querySelectorAll('.sub')
+const gnb_li_m = nav_m.querySelectorAll('.gnb > li')
+console.log(sub_m, gnb_li_m)
+for(let i of sub_m){i.style.display = 'none'}
+for(let i of gnb_li_m){
+    console.log(i)
+    i.addEventListener('click',()=>{
+        console.log(i.children[1])
+        for(let j of sub_m){j.style.display = 'none'}
+        i.children[1].style.display = 'block' // flex로 디자인 시 'flex', grid로 디자인 시 'gird'
+    })
+}
+// 3. 모바일 주 메뉴 클릭 시
+// 3-1. 모든 서브 메뉴 숨기기
+// 3-2. 해당 주 메뉴의 서브 메뉴 보이기
--- project/casper/script/script_jq.js
+++ project/casper/script/script_jq.js
@@ -21,3 +21,37 @@ $nav.on('mouseout',function(){
     $sub_bg.stop().slideUp(300)
     $sub.stop().slideUp(300)
 })
+
+// ============= 모바일 메뉴
+const $all_nav_m = $('.all_nav')
+const $nav_m = $('.m_nav nav')
+const $sub_m = $('.m_nav .sub')
+const $gnb_li_m = $('.m_nav .gnb > li')
+console.log($all_nav_m, $nav_m, $sub_m, $gnb_li_m)
+// 제이쿼리 반복문 for, 조건문 if, 삼항조건, 함수 작성방법 js와 동일
+
+// 제이쿼리 클래스 적용 명령
+// js(classList.add) -> jq(addClass(클래스명))
+// js(classList.toggle) -> jq(toggleClass(클래스명))
+// 제이쿼리객체.css('속성','값').css('속성','값');
+// 제이쿼리객체.css({'속성','값'});
+// 제이쿼리객체.find('자식 자손 대상')
+// 제이쿼리객체.eq(인덱스)
+$nav_m.addClass('m_nav_hide')
+// 1. 모바일 전체메뉴 아이콘(m_nav > all_nav) 클릭 시
+$sub_m.hide()
+// 1-1. 모바일 nav가(m_nav > nav) 보이는 상태라면 -> 숨기기
+// 1-2. 모바일 nav가 숨겨진 상태라면 -> 보이기
+$all_nav_m.on('click',function(e){
+    e.preventDefault();
+    $nav_m.toggleClass('m_nav_hide')
+})
+// 2. 모바일 서브메뉴 모두 숨기기(기본)
+// 3. 모바일 주 메뉴 클릭 시
+// 3-1. 모든 서브 메뉴 숨기기
+// 3-2. 해당 주 메뉴의 서브 메뉴 보이기
+$gnb_li_m.on('click',function(){
+    $sub_m.hide()
+    // $(this)란? 클릭 이벤트 현재 대상
+    $(this).find('.sub').show()
+})
